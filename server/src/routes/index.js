4e3bc7fa4af32eddb3d14e4dbed7649668c936b2
dannyDv23 <choecl263@gmail.com>
2024-09-21 12:14:05 +0700

    Config(CI/CD) -Completed

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Continuous Deployment Completed!");
+  res.send("Continuous Deployment Successfully!");
 });
 
 module.exports = router;
