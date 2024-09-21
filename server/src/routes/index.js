6721c282f75c5797a17ff2dddbb1e237c813fe34
dannyDv23 <choecl263@gmail.com>
2024-09-21 11:12:45 +0700

    hotfix(CI/CD)- Fix check-log stage & test

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Continuous Deployment completed successfully!");
 });
 
 module.exports = router;
