c6232de60461e2fc52481692e27bc9287cbed33f
dannyDv23 <choecl263@gmail.com>
2024-09-21 11:59:48 +0700

    Fix check-log stage & test

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Continuous Deployment completed successfully!");
+  res.send("Continuous Deployment Successfully!");
 });
 
 module.exports = router;
