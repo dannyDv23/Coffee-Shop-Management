566aa3744d60165f03c100d863979254532bbd95
dannyDv23 <choecl263@gmail.com>
2024-09-25 21:40:27 +0700

    feature(Register) -Refactor logic in Controller

--- server/src/services/employee.service.js
+++ server/src/services/employee.service.js
@@ -1,13 +1,7 @@
 const Employee = require("../models/employee");
 const Auth = require("../models/auth");
-const ApiError = require("../utils/ApiError");
-const httpStatus = require("http-status");
 
 const createEmployee = async (employeeBody) => {
-  const existingUser = await Auth.findOne({ username: employeeBody.username });
-  if (existingUser) {
-    throw new ApiError(httpStatus.BAD_REQUEST, "Username already taken");
-  }
   const employee = await Employee.create({
     name: employeeBody.name,
     // Add other necessary fields
@@ -20,12 +14,11 @@ const createEmployee = async (employeeBody) => {
 
   return { employee, authRecord };
 };
-const getEmployeeById = async (id) => {
-  return await Employee.findById(id);
-
+const getEmployeeByUsername = async (username) => {
+  return await Auth.findOne({ username });
 };
 
 module.exports = {
   createEmployee,
-  getEmployeeById,
+  getEmployeeByUsername,
 };
