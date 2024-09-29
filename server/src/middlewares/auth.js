4f7a070fb808e43ec8f8baee162567c9e23ab427
dannyDv23 <choecl263@gmail.com>
2024-09-29 10:24:48 +0700

    feat: Authorization, Update route handle.

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,19 +1,19 @@
-const ApiError = require('./../utils/ApiError');
-const httpStatus = require('http-status');
+const ApiError = require("./../utils/ApiError");
+const httpStatus = require("http-status");
 const verifyCallBack = (req, resolve, reject) => async (err, user, info) => {
   if (err || info || !user) {
-    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'please authenticate'));
+    return reject(new ApiError(httpStatus.UNAUTHORIZED, "please authenticate"));
   }
 
   req.user = user;
   resolve();
 };
 
-const passport = require('passport');
+const passport = require("passport");
 const auth = async (req, res, next) => {
   return new Promise((resolve, reject) => {
     passport.authenticate(
-      'jwt',
+      "jwt",
       { session: false },
       verifyCallBack(req, resolve, reject)
     )(req, res, next);
@@ -22,4 +22,21 @@ const auth = async (req, res, next) => {
     .catch((error) => next(error));
 };
 
-module.exports = auth;
+const roleFilter = (roles = []) => {
+  if (typeof roles === "string") {
+    roles = [roles];
+  }
+
+  return (req, res, next) => {
+    if (roles.length > 0 && !roles.includes(req.user.role)) {
+      return next(new ApiError(httpStatus.FORBIDDEN, "Forbidden! Role not allowed"));
+    }
+    return next();
+  };
+};
+
+
+module.exports = {
+  auth,
+  roleFilter,
+};
